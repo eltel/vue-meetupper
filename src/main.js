400a62edca237142c835d0234ec2686f6b238ccd
Terry Mitchell <djterrymitchell@gmail.com>
2019-08-10 12:17:46 -0300

    latest version, with errors

--- src/main.js
+++ src/main.js
@@ -1,13 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
-import AppDropdown from './components/shared/AppDropdown'
-import AppHero from './components/shared/AppHero'
+import Vue from "vue";
+import moment from "moment";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import router from "./router";
+import AppDropdown from "./components/shared/AppDropdown";
+import AppHero from "./components/shared/AppHero";
 
-Vue.component('AppHero', AppHero)
-Vue.component('AppDropdown', AppDropdown)
+Vue.config.productionTip = false;
+
+Vue.component("AppHero", AppHero);
+Vue.component("AppDropdown", AppDropdown);
+
+Vue.filter("capitalize", function(value) {
+  if (value && typeof value === "string") {
+    return value.charAt(0).toUpperCase() + value.slice(1);
+  }
+
+  return "";
+});
+
+Vue.filter("formatDate", function(value, formatType = "LL") {
+  if (!value) return "";
+
+  return moment(value).format(formatType);
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
